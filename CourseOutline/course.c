32f8816fb9ddd78356dfc9b9e368a005e9e1c1d0
wangcheng <1739965843@qq.com>
2026-08-09 17:15:20 +0800

    Create course.c

---
+++ CourseOutline/course.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int x;
+    int n = 1;
+    scanf_s("%d", &x);
+    if (x > 999) {
+        n = 4;
+    }
+    else if (x > 99) {
+        n = 3;
+    }
+    else if (x > 9) {
+        n = 2;
+    }
+    else {
+        n = 1;
+    }
+    printf("%d\n", n);
+
+    return 0;
+   
+}
