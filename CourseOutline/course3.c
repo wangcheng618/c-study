ab1ad4f7db77f95b1c0f04a71cfe9831c6c9983d
wangcheng <1739965843@qq.com>
2026-08-09 17:58:14 +0800

    Create course3.c

---
+++ CourseOutline/course3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int x;
+    int n = 0;
+    scanf_s("%d", &x);
+    do
+    {
+        x /= 10;
+        n++;
+    } while (x > 0);
+
+    printf("%d\n", n);
+
+    return 0;
+   
+}
