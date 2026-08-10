ce731f90928068c80c1cf49645e1bb439401ddc5
wangcheng <1739965843@qq.com>
2026-08-10 09:25:39 +0800

    Create course10.c

---
+++ CourseOutline/course10.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    int x;
+    //scanf_s("%d", &x);
+    for (x = 2;x < 100; x++) {
+
+        int i;
+        int isPrime = 1;//x是素数
+        for (i = 2;i < x;i++) {
+            if (x % i == 0) {
+                isPrime = 0;
+                break;
+            }
+        }if (isPrime == 1) {
+            printf("%d", x);
+        }
+    }
+    printf("\n");
+
+    return 0;
+}
