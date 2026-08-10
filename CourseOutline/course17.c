28cb6cda3201eca90b2277c640e7564ba687da4f
wangcheng <1739965843@qq.com>
2026-08-10 10:01:33 +0800

    Create course17.c

---
+++ CourseOutline/course17.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+    int x;
+    scanf_s("%d", &x);
+
+    int t = 0;
+    do {
+        int d = x % 10;
+        t = t * 10 + d;
+        x /= 10;
+    } while (x > 0);
+    printf("x = %d,t=%d\n", x, t);
+    x = t;
+    do {
+        int d = x % 10;
+        printf("%d", d);
+        if (x > 9) {
+            printf("");
+        }
+        x /= 10;
+    } while (x > 0);
+    printf("\n");
+
+    return 0;
+}
