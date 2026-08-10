47a6f9ec2a18f4fabacf59f19e6fd1cdd13e4d05
wangcheng <1739965843@qq.com>
2026-08-10 15:10:30 +0800

    Create course26.c

---
+++ CourseOutline/course26.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int dividend, divisor;
+    scanf_s("%d/%d", &dividend, &divisor);
+
+    int a = dividend;
+    int b = divisor;
+    int t;
+    while (b > 0) {
+        t = a % b;
+        a = b;
+        b = t;
+    }
+    printf("%d%/%d\n", dividend / a, divisor / a);
+
+    return 0;
+}
