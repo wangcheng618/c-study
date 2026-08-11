225068fb05966166d9ad8532a1b0ddd71ef55c66
wangcheng <1739965843@qq.com>
2026-08-11 17:41:31 +0800

    Update course2.c

--- CourseOutline/course2.c
+++ CourseOutline/course2.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
-
 int main()
 {
-    int x;
+    long long x;  // 改用64位长整型，范围约 ±9e18
     int n = 0;
 
-    scanf_s("%d", &x);
-    n++;
-    x /= 10;
-    while (x > 0) {
-        n++;
-        x /= 10;
-    }
-    printf("%d\n", n);
+    scanf_s("%lld", &x);
 
+    // 处理输入为0的特殊情况（0也是1位数）
+    if (x == 0) {
+        n = 1;
+    }
+    else {
+        while (x > 0) {
+            n++;
+            x /= 10;
+        }
+    }
+
+    printf("位数是：%d\n", n);
     return 0;
-   
 }
