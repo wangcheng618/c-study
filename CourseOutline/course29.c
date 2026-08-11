9ffc95452ad18f647422b006d411ee5667c7bcff
wangcheng <1739965843@qq.com>
2026-08-11 15:07:12 +0800

    Update course29.c

--- CourseOutline/course29.c
+++ CourseOutline/course29.c
@@ -1,18 +1,11 @@
 #include <stdio.h>
 int main()
 {
-    int a, n;
-    scanf_s("%d %d", &a, &n);
+    int cm = 0;
+    scanf_s("%d", &cm);
 
-    int sum = 0;
-    int i;
-    int t = 0;
-    // 0*10+2 2*10+2 (2*10+2)*10+2
-    for (i = 0;i < n;i++) {
-        t = t * 10 + a;
-        sum += t;
-    }
-    printf("%d\n", sum);
+    int foot = cm / 30.48;
+    int inch = ((cm / 30.48) - foot)*12;
 
     return 0;
 }
