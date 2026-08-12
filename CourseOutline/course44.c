ec8a47fd77f954c86176a74ce04296079c4d685f
wangcheng <1739965843@qq.com>
2026-08-12 16:19:02 +0800

    Update course44.c

--- CourseOutline/course44.c
+++ CourseOutline/course44.c
@@ -5,7 +5,7 @@ void sum(int begin, int end)
     int i;
     int sum = 0;
     for (i = begin;i <= end;i++) {
-        sum += 1;
+        sum += i;
     }
     printf("%d到%d的和是%d\n", begin, end, sum);
 
